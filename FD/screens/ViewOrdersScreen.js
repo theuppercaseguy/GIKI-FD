d629e7bfa89f3db2708cf9a7644f459f63bd89db
theuppercaseguy <saadan06@gmail.com>
2023-06-10 02:17:27 +0500

    placedorderscreen added, And working, items deleting

--- FD/screens/ViewOrdersScreen.js
+++ FD/screens/ViewOrdersScreen.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
-import { addDoc, collection, query, where, getDocs } from 'firebase/firestore';
+import { addDoc, collection, query, where,deleteDoc,Timestamp, getDocs } from 'firebase/firestore';
 import { db, auth, fbauth, storage } from '../firebaseauth';
 
 import { CartContext } from './CartContext';
 
 const ViewOrdersScreen = ({ navigation }) => {
   const [orders, setOrders] = useState([]);
+  const [deleteLoading, setDeleteLoading] = useState(false);
+  const [deleteMessage, setDeleteMessage] = useState('');
 
   const fetchOrders = async () => {
     const currentUser = auth.currentUser;
@@ -55,55 +57,130 @@ const ViewOrdersScreen = ({ navigation }) => {
     navigation.navigate('UserScreen');
   };
 
+
+  const handleDeleteOrder = (timestamp) => {
+    setDeleteLoading(true);
+    setDeleteMessage('Deleting...');
+  
+    // Get the document reference based on the conditions
+    const ordersRef = collection(db, 'Orders');
+  
+    const queryRef = query(
+      ordersRef,
+      where('Name', '==', auth.currentUser.displayName),
+      where('isConfirmed', '==', 'delivered'),
+      where('Time', '==', timestamp)
+    );
+  
+    getDocs(queryRef)
+      .then((querySnapshot) => {
+        if (!querySnapshot.empty) {
+          const docRef = querySnapshot.docs[0].ref;
+  
+          // Delete the document
+          deleteDoc(docRef)
+            .then(() => {
+              setDeleteLoading(false);
+              setDeleteMessage('Order deleted successfully');
+              // Refresh the orders after deletion
+              fetchOrders();
+            })
+            .catch((error) => {
+              setDeleteLoading(false);
+              setDeleteMessage('Error deleting order.');
+              console.log('Error deleting order: ' + error.message);
+            });
+        } else {
+          setDeleteLoading(false);
+          setDeleteMessage('Order not found');
+        }
+      })
+      .catch((error) => {
+        setDeleteLoading(false);
+        setDeleteMessage('Error retrieving order. ');
+        console.log('Error retrieving order: ' + error.message);
+      });
+  };
+  
+
   return (
     <View style={styles.container}>
       <View style={styles.topBar}>
         <Text style={styles.title}>Placed Orders</Text>
-        <Button title="Sign Out" style={styles.Buttons} onPress={handleLogout} color="#0782F9" />
+        <Button title="Sign Out" onPress={handleLogout} color="#0782F9" />
       </View>
 
-      <ScrollView style={styles.ordersContainer}>
-        {orders.map((order, index) => {
-          if (order.isConfirmed === "delivered") {
-            return null; // Skip orders with "delivered" status
-          }
+      <View style={{ flex: 1 }}>
+        <ScrollView style={styles.ordersContainer}>
+          {deleteMessage ? <Text>{deleteMessage}</Text> : null}
+          {orders.map((order, index) => {
+            if (order.isConfirmed === "delivered") {
+              return (
+                <View key={order.Name} style={styles.orderItem}>
+                  <View style={styles.orderheadingContainer}>
+                    <Text style={styles.orderHeading}>Past Order # {(index - orders.length) * -1}</Text>
+                    <Text style={styles.orderTimeContainer}>
+                      {order.Time.toDate().toLocaleDateString()} {'\n '}{order.Time.toDate().toLocaleTimeString()}
+                    </Text>
+                  </View>
+                  {order.FoodItems.map((foodItem, foodIndex) => (
+                    <View key={foodItem.category} style={styles.foodItem}>
+                      <Text style={styles.foodItemHeading}>Food Item # {foodIndex + 1}</Text>
+                      <View style={styles.foodItemDetails}>
+                        <Text>Name: {foodItem.name} x {foodItem.amount}</Text>
+                        <Text style={styles.individualPrice}>Price: {foodItem.price}/-</Text>
+                      </View>
+                    </View>
+                  ))}
+                  <Button
+                    title={deleteLoading ? 'Deleting...' : 'Delete'}
+                    onPress={() => handleDeleteOrder(order.Time)}
+                    // style={styles.deleteButton}
+                    color={deleteLoading ?  "#bf3a3a" : "#ff0000"}
+                  />
 
-          return (
-            <View key={order.Name} style={styles.orderItem}>
-
-              <View style={styles.orderheadingContainer}>
-                <Text style={styles.orderHeading}>Order # {index + 1}</Text>
-                <View style={styles.orderTimeContainer}>
-                  <Text style={styles.orderTimeText}>{order.Time.toDate().toLocaleDateString()} {'\n'} {order.Time.toDate().toLocaleTimeString()}</Text>
                 </View>
-              </View>
+              );
+            }
 
-              {order.FoodItems.map((foodItem, foodIndex) => (
-                <View key={foodItem.category} style={styles.foodItem}>
-                  <Text style={styles.foodItemHeading}>Food Item # {foodIndex + 1}</Text>
-                  <View style={styles.foodItemDetails}>
-                    <Text>Name: {foodItem.name} x {foodItem.amount}</Text>
-                    <Text style={styles.individualPrice}>Price: {foodItem.price}/-</Text>
+            return (
+              <View key={order.Name} style={styles.orderItem}>
+                <View style={styles.orderheadingContainer}>
+                  <Text style={styles.orderHeading}>Order # {index + 1}</Text>
+                  <View style={styles.orderTimeContainer}>
+                    <Text style={styles.orderTimeText}>
+                      {order.Time.toDate().toLocaleDateString()} {'\n '}
+                      {order.Time.toDate().toLocaleTimeString()}
+                    </Text>
                   </View>
                 </View>
-              ))}
-              <View style={styles.orderSummary}>
-                <View style={styles.orderStatusContainer}>
-                  <Text style={styles.orderStatusText}>Order Status: {order.isConfirmed}</Text>
-                </View>
-                <View style={styles.totalPriceContainer}>
-                  <Text style={styles.totalPriceText}>Total Price: {order.TotalPrice}</Text>
+
+                {order.FoodItems.map((foodItem, foodIndex) => (
+                  <View key={foodItem.category} style={styles.foodItem}>
+                    <Text style={styles.foodItemHeading}>Food Item # {foodIndex + 1}</Text>
+                    <View style={styles.foodItemDetails}>
+                      <Text>Name: {foodItem.name} x {foodItem.amount}</Text>
+                      <Text style={styles.individualPrice}>Price: {foodItem.price}/-</Text>
+                    </View>
+                  </View>
+                ))}
+                <View style={styles.orderSummary}>
+                  <View style={styles.orderStatusContainer}>
+                    <Text style={styles.orderStatusText}>Order Status: {order.isConfirmed}</Text>
+                  </View>
+                  <View style={styles.totalPriceContainer}>
+                    <Text style={styles.totalPriceText}>Total Price: {order.TotalPrice}</Text>
+                  </View>
                 </View>
               </View>
+            );
+          })}
+        </ScrollView>
 
-            </View>
-          );
-        })}
-      </ScrollView>
-
-      <View style={styles.bottomBar}>
-        <Button style={styles.Buttons} title="Go to Cart" onPress={handleGoToCart} color="#999999" />
-        <Button style={styles.Buttons} title="Go to Menu" onPress={handleGoToMenu} color="#999999" />
+        <View style={styles.bottomBar}>
+          <Button style={styles.Buttons} title="Go to Cart" onPress={handleGoToCart} color="#999999" />
+          <Button style={styles.Buttons} title="Go to Menu" onPress={handleGoToMenu} color="#999999" />
+        </View>
       </View>
     </View>
   );
@@ -114,6 +191,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
+  },
+  deleteButton: {
+    marginTop: 10,
+    backgroundColor: 'red',
+    borderRadius: 5,
+    padding: 10,
   },
   orderTimeContainer: {
     alignItems: 'flex-end',
@@ -140,12 +223,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
     // textAlign: "center",
-
   },
   orderItem: {
     marginBottom: 10,
     backgroundColor: '#FFFFFF',
     padding: 10,
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
   },
   foodItem: {
     marginBottom: 10,
@@ -157,7 +241,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 10,
-    flexDirection: "row",
     justifyContent: "space-between",
   },
   orderStatusContainer: {
@@ -198,12 +281,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
   },
-  orderItem: {
-    marginBottom: 16,
-    borderWidth: 1,
-    borderColor: '#CCCCCC',
-    padding: 16,
-  },
   bottomBar: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -212,5 +289,6 @@ const styles = StyleSheet.create({
     padding: 16,
   },
 });
+
 
 export default ViewOrdersScreen;
